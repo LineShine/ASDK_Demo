edf04d0f11a8b640b7fd6d9806361a89ff1f1f1b
李国平 <liguoping@liguopingdeMBP.lan>
2017-04-15 19:41:16 +0800

    Project Create

---
+++ ASDK_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASDK_Demo
+//
+//  Created by 李国平 on 2017/4/14.
+//  Copyright © 2017年 gpl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASDK_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASDK_Demo
+//
+//  Created by 李国平 on 2017/4/14.
+//  Copyright © 2017年 gpl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
